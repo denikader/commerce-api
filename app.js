732dee3c14c4fc99dac34d8494fc465130ee771e
deni Kader <92921224+denikader@users.noreply.github.com>
2021-12-09 12:34:31 +0300

    mongoose setup

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import userRouter from "./src/routes/user.routes.js";
+import mongoose from "mongoose";
 
-const app = express();
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/commerce_api");
+  const app = express();
 
-// setup routes
-app.use(userRouter);
-app.listen(5000, () => {
-  console.log("listening. on http://localhost:5000");
-});
+  //parse the body am dera yak jar anusret
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  // setup routes
+  app.use(userRouter);
+
+  app.listen(5000, () => {
+    console.log("listening. on http://localhost:5000");
+  });
+}
+
+main();
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import User from "../models/user.model.js";
 
 const userRouter = Router();
 
 userRouter.get("/users", (rea, res) => {
-  res.send("hello");
+  res.send("hello User");
 });
 
+userRouter.post("/users", async (req, res) => {
+  const user = new User(req, body);
+  await user.save();
+  res.json({ massage: "User Crated" });
+});
 export default userRouter;
